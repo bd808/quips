bd808/quips

commit 4e6ef11cffc57d22bf1c07266020b7045f4589db

Fix codesniffer warnings
Mostly boring, but replacing $_GET and $_POST with
filter_input_array(INPUT_{GET,POST}) may or may not work as hoped.

src/Pages/Search.php
@@ -34,7 +34,7 @@ class Search extends Page {
 		$this->form->expectInt( 'i',
 			[ 'min_range' => 1, 'max_range' => 200, 'default' => 20 ]
 		);
-		$this->form->validate( $_GET );
+		$this->form->validate( filter_input( INPUT_GET ) );
 
 		$params = [
 			'query' => $this->form->get( 'q' ),

src/App.php
@@ -301,7 +301,7 @@ class App extends AbstractApp {
 				App::template( $slim, 'about' );
 				App::template( $slim, 'help' );
 			}
-		); // end group '/'
+		);
 
 		$slim->group( '/quip/',
 			$middleware['inject-user'],
@@ -376,5 +376,5 @@ class App extends AbstractApp {
 		$slim->notFound( function () use ( $slim ) {
 			$slim->render( '404.html' );
 		} );
-	} // end configureRoutes
+	}
 }

src/Auth/OAuthUser.php
@@ -38,6 +38,12 @@ class OAuthUser implements UserData {
 	 */
 	protected $attributes;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param Token $token
+	 * @param object $attributes
+	 */
 	public function __construct( Token $token, $attributes ) {
 		$this->token = $token;
 		$this->attributes = $attributes;
@@ -51,6 +57,10 @@ class OAuthUser implements UserData {
 		return $this->attributes->sub;
 	}
 
+	/**
+	 * Get username
+	 * @return string
+	 */
 	public function getName() {
 		return $this->attributes->username;
 	}

src/Auth/OAuthUserManager.php
@@ -42,8 +42,8 @@ class OAuthUserManager implements LoggerAwareInterface, UserManager {
 	protected $oauth;
 
 	/**
-	 * @param Client $oauth
-	 * @param LoggerInterface $logger
+	 * @param Client|null $client
+	 * @param LoggerInterface|null $logger
 	 */
 	public function __construct(
 		Client $client = null,

src/Page.php
@@ -32,8 +32,8 @@ class Page extends Controller {
 	protected $quips;
 
 	/**
-	* @param Quips $quips
-	*/
+	 * @param Quips $quips
+	 */
 	public function setQuips( Quips $quips ) {
 		$this->quips = $quips;
 	}

src/Pages/Delete.php
@@ -27,6 +27,11 @@ use Bd808\Bash\Page;
  */
 class Delete extends Page {
 
+	/**
+	 * Handle POST requests.
+	 *
+	 * @param string $id Quip Id
+	 */
 	protected function handlePost( $id ) {
 		$redir = $this->urlFor( 'home' );
 		$ok = $this->quips->delete( $id );

src/Pages/Edit.php
@@ -21,13 +21,17 @@ namespace Bd808\Bash\Pages;
 
 use Bd808\Bash\Page;
 
-use Elastica\Result;
-
 /**
  * @author Bryan Davis <bd808@wikimedia.org>
  * @copyright © 2015 Bryan Davis and contributors.
  */
 class Edit extends Page {
+
+	/**
+	 * Setup input form.
+	 *
+	 * @param array $defaults
+	 */
 	protected function setupForm( $defaults = [] ) {
 		$defaults = array_merge( [
 			'@timestamp' => date( 'c' ),
@@ -67,6 +71,11 @@ class Edit extends Page {
 		$this->view->set( 'form', $this->form );
 	}
 
+	/**
+	 * Handle GET requests.
+	 *
+	 * @param string $id Quip Id
+	 */
 	protected function handleGet( $id ) {
 		$defaults = [];
 		if ( $id === 'new' ) {
@@ -89,6 +98,11 @@ class Edit extends Page {
 		$this->render( 'quip/edit.html' );
 	}
 
+	/**
+	 * Handle POST requests.
+	 *
+	 * @param string $id Quip Id
+	 */
 	protected function handlePost( $id ) {
 		$this->setupForm();
 		$redir = $this->urlFor( 'edit', [ 'id' => $id ] );

src/Pages/OAuth.php
@@ -32,7 +32,7 @@ use MediaWiki\OAuthClient\Token;
  */
 class OAuth extends Page {
 
-	const REQEST_KEY = 'oauthreqtoken';
+	public const REQEST_KEY = 'oauthreqtoken';
 
 	/**
 	 * @var Client $oauth
@@ -44,14 +44,29 @@ class OAuth extends Page {
 	 */
 	protected $manager;
 
+	/**
+	 * Set OAuth client.
+	 *
+	 * @param Client $oauth
+	 */
 	public function setOAuth( Client $oauth ) {
 		$this->oauth = $oauth;
 	}
 
+	/**
+	 * Set user manager object.
+	 *
+	 * @param OAuthUserManager $manager
+	 */
 	public function setUserManager( OAuthUserManager $manager ) {
 		$this->manager = $manager;
 	}
 
+	/**
+	 * Handle GET requests.
+	 *
+	 * @param string $stage
+	 */
 	protected function handleGet( $stage ) {
 		switch ( $stage ) {
 			case 'callback':
@@ -98,7 +113,7 @@ class OAuth extends Page {
 		$this->form->requireString( 'oauth_verifier' );
 		$this->form->requireInArray( 'oauth_token', [ $key ] );
 
-		if ( $this->form->validate( $_GET ) ) {
+		if ( $this->form->validate( filter_input_array( INPUT_GET ) ) ) {
 			$verifyCode = $this->form->get( 'oauth_verifier' );
 			try {
 				$accessToken = $this->oauth->complete( $token, $verifyCode );

commit 4e8a6bf7d4c072b75111a2d43b4afcf6589a350a

Use filter_input_array, not filter_input
Typo introduced in 4e6ef11cffc57d22bf1c07266020b7045f4589db.

Bug: T243030

src/Pages/Search.php
@@ -34,7 +34,7 @@ class Search extends Page {
 		$this->form->expectInt( 'i',
 			[ 'min_range' => 1, 'max_range' => 200, 'default' => 20 ]
 		);
-		$this->form->validate( filter_input( INPUT_GET ) );
+		$this->form->validate( filter_input_array( INPUT_GET ) );
 
 		$params = [
 			'query' => $this->form->get( 'q' ),